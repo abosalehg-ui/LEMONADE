7185772b3f5a40589bf6cc8cef8f0cba7b25203a
abosalehg-ui <abo.saleh.g@gmail.com>
2025-11-01 04:59:36 +0300

    Update game.js

--- game.js
+++ game.js
@@ -210,7 +210,7 @@ class LemonadeStandScene extends Phaser.Scene {
         this.customerGroup.add(customer);
 
         // الحركة إلى نقطة البيع (وسط الشاشة)
-        const targetX = 210;
+        const targetX = 190;
         const targetY = 210;
         
         this.tweens.add({
